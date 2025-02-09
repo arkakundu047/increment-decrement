296cbf9f86dfe445fa8060aff3dd1f8766b73f26
arkakundu047 <arkakundu1969@gmail.com>
2025-02-09 20:54:04 +0530

    counter changes

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Counter.css'
 
 const Counter = () => {
     const [num,setnum]=useState(0);
+    const [data,setdata]=useState("student");
     const incr=()=>{
         setnum(num+1);
     }
@@ -14,17 +15,25 @@ const Counter = () => {
         else{
             setnum(num - 1);
         }
-      
+      const Change=()=>{
+        setdata("Arka Kundu");
+      }
     };
+
+    useEffect(()=>{
+        alert("running useeffect")
+    },[data])
   return (
     <>
       <div className="conatiner">
+        <h1>{data}</h1>
         <h1>{num}</h1>
         <div className="btn">
           <button id="btn1" onClick={incr}>
             Increment
           </button>
           <button onClick={decr}>Decrement</button>
+          <button onClick={Change}>Change</button>
         </div>
       </div>
     </>
